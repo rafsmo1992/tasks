9e412ca93c19b2d0bf7270829c1e5e7ea56a9a67
Rafal Smolaga <rafsmo1992@gmail.com>
2023-01-16 17:34:02 +0100

    test

--- src/main/java/com/crud/tasks/service/TrelloService.java
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -10,14 +10,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
-import static java.util.Optional.ofNullable;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class TrelloService {
 
-    public static final String SUBJECT = "Tasks: New Trello card:";
+    public static final String SUBJECT = "Tasks: New Trello card";
     private final TrelloClient trelloClient;
     private final SimpleEmailService emailService;
     private final AdminConfig adminConfig;
@@ -26,16 +25,16 @@ public class TrelloService {
         return trelloClient.getTrelloBoards();
     }
 
+
     public CreatedTrelloCard createTrelloCard(final TrelloCardDto trelloCardDto) {
         CreatedTrelloCard newCard = trelloClient.createNewCard(trelloCardDto);
-        ofNullable(newCard).ifPresent(card -> emailService.send(
-                new Mail(
-                        adminConfig.getAdminMail(),
-                        SUBJECT,
-                        "New card: " + trelloCardDto.getName() + " has been created on your Trello account"
-                )));
+        Optional.ofNullable(newCard).ifPresent(card -> emailService.send(
+                Mail.builder()
+                        .mailTo(adminConfig.getAdminMail())
+                        .subject(SUBJECT)
+                        .message("New card: " + trelloCardDto.getName() + " created Trello account")
+                        .build()
+        ));
         return newCard;
     }
-
-
 }
